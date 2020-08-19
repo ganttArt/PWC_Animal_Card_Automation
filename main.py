3e42ce3ef92ba31bb5d018bf550306700608d7b7
Chris Gantt <gantt.art@gmail.com>
2020-08-19 00:18:14 -0700

    Card generation by Animal ID added

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import os
 import io
+import requests
+import json
 from datetime import datetime
 from PIL import Image, UnidentifiedImageError
+from io import BytesIO
 from flask import Flask, render_template, request, send_file
 from card_generation_functions import create_card, create_pdf
 
@@ -9,8 +12,40 @@ from card_generation_functions import create_card, create_pdf
 APP = Flask(__name__)
 # APP.secret_key = os.environ.get('SECRET_KEY').encode()
 
-
 @APP.route('/', methods=['GET', 'POST'])
+def animal_id_entry():
+    if request.method == 'POST':
+        try:
+            animal_id = int(request.form['animal_id'])
+            animal_info = requests.get(f'https://pwc-volunteers-backend.herokuapp.com/animals/card_generator/{animal_id}')
+            if animal_info.text == '':
+                return render_template('animal_id_entry.jinja2', error='Animal id is not in PWC database')
+        except ValueError as ex:
+            print(ex)
+            return render_template('animal_id_entry.jinja2', error='Animal Id must be a number')
+        
+        animal_info = json.loads(animal_info.text)
+
+        shelter_name = animal_info['shelter']['name']
+        shelter_email = animal_info['shelter']['email']
+        shelter_phone = animal_info['shelter']['phone_number']
+        animal_description = animal_info['description']
+        
+        image = requests.get(animal_info['photos'][0]['url'])
+        image = Image.open(BytesIO(image.content))
+        
+        card = create_card(image, shelter_name, shelter_email, shelter_phone, animal_description)
+        pdf = create_pdf(card)
+
+        output = io.BytesIO()
+        pdf.convert('RGB').save(output, format='PDF')
+        output.seek(0, 0)
+        return send_file(output, mimetype='application/pdf', as_attachment=False)
+    else:
+        return render_template('animal_id_entry.jinja2')
+
+
+@APP.route('/manual_entry', methods=['GET', 'POST'])
 def manual_entry():
     if request.method == 'POST':
         try:
