74583eace6622dd1938951cf31e92d7046eedc19
Chris Gantt <gantt.art@gmail.com>
2020-11-24 08:03:43 -0800

    Request shelter data from api, and loading animation screen for long transitions.

--- main.py
+++ main.py
@@ -6,11 +6,27 @@ from datetime import datetime
 from PIL import Image, UnidentifiedImageError
 from io import BytesIO
 from flask import Flask, render_template, request, send_file
+from werkzeug.exceptions import BadRequestKeyError
 from card_generation_functions import create_card, create_pdf
 
 
 app = Flask(__name__)
-# APP.secret_key = os.environ.get('SECRET_KEY').encode()
+
+
+def load_shelters():
+    """
+        Makes a request to pwc database to get all shelter information.
+        Returns a dictionary where the Key = Shelter name, and Value = {'phone_number', 'email'}
+    """
+    shelters = {}
+    response = requests.get('https://pwc-volunteers-backend.herokuapp.com/shelters')
+    shelter_list = json.loads(response.text)
+
+    for shelter in shelter_list:
+        shelters[shelter['name']] = {'phone_number': shelter['phone_number'], 'email': shelter['email']}
+
+    return shelters
+
 
 @app.route('/', methods=['GET', 'POST'])
 def animal_id_entry():
@@ -47,16 +63,20 @@ def animal_id_entry():
 
 @app.route('/manual_entry', methods=['GET', 'POST'])
 def manual_entry():
+    SHELTERS = load_shelters()
     if request.method == 'POST':
         try:
             image = Image.open(request.files['file'])
+            shelter_name = request.form['shelter']
         except UnidentifiedImageError as ex:
             print(ex)
-            return render_template('manual_entry.jinja2', error='Image error: Please try again.')
+            return render_template('manual_entry.jinja2', error='Image error: Please try again.', shelters=SHELTERS)
+        except BadRequestKeyError as ex:
+            print(ex)
+            return render_template('manual_entry.jinja2', error='Please choose a shelter, and try again.', shelters=SHELTERS)
 
-        shelter_name = request.form['shelter_name']
-        shelter_email = request.form['shelter_email']
-        shelter_phone = request.form['shelter_phone']
+        shelter_email = SHELTERS[shelter_name]['email']
+        shelter_phone = SHELTERS[shelter_name]['phone_number']
         animal_description = request.form['description']
 
         card = create_card(image, shelter_name, shelter_email, shelter_phone, animal_description)
@@ -67,9 +87,10 @@ def manual_entry():
         output.seek(0, 0)
         return send_file(output, mimetype='application/pdf', as_attachment=False)
     else:
-        return render_template('manual_entry.jinja2')
+        print('get request')
+        return render_template('manual_entry.jinja2', shelters=SHELTERS)
 
 
 if __name__ == "__main__":
     PORT = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=PORT, debug=False)
+    app.run(host='0.0.0.0', port=PORT, debug=True)    
